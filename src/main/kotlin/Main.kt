318ef6e94e3b439cb3eb7a2801b1b2e6b20bf220
Artyom Aleksyuk <artyom.h31@gmail.com>
2018-06-28 19:00:05 +0300

    Clean up code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,61 @@
 import java.io.File
-import java.nio.CharBuffer
+import java.io.Reader
+import java.io.Writer
 
 fun main(args: Array<String>) {
-    val output = File("/tmp/wrapperfifo_input").writer()
-    println("Output opened!")
-    val input = File("/tmp/wrapperfifo_output").reader()
-    println("Input opened!")
-//    val size = input.readLine()
-//    val data = input.readLine()
-//    println(size)
-//    println(buffer)
-//    println(data)
-//    output.write("Hi!\n")
-    output.write("open(\"/proc/swaps\").read()\n")
-    output.flush()
-    output.close()
-    val buffer = CharArray(1024)
-    val size = input.read(buffer)
-    val actualData = buffer.copyOfRange(0, size)
-    println(actualData)
-    input.close()
+    val requests = Requests()
+    requests.get("https://api.github.com/user")
+    requests.stopPython()
+}
+
+class Requests {
+    val output: Writer
+    val input: Reader
+    private val pythonProcess: Process
+
+    init {
+        pythonProcess = ProcessBuilder("python3", "/home/artyom/Projects/HTTPClientKotlin/src/main/python/main.py")
+                .redirectError(ProcessBuilder.Redirect.INHERIT)
+                .start()
+        runPython()
+        output = File("/tmp/wrapperfifo_input").writer()
+        println("Output opened!")
+        input = File("/tmp/wrapperfifo_output").reader()
+        println("Input opened!")
+    }
+
+    fun runPython() {
+        val greeting = pythonProcess.inputStream.bufferedReader().readLine()
+        check(greeting == "Done")
+        println("Greeting received")
+
+        //    val size = input.readLine()
+        //    val data = input.readLine()
+        //    println(size)
+        //    println(buffer)
+        //    println(data)
+        //    output.write("Hi!\n")
+        //    output.write("open(\"/proc/swaps\").read()\n")
+    }
+
+    fun stopPython() {
+        println("Stop Python")
+        output.close()
+        input.close()
+        pythonProcess.destroy()
+    }
+
+    fun printResponse() {
+        val buffer = CharArray(1024)
+        val size = input.read(buffer)
+        val actualData = buffer.copyOfRange(0, size)
+        println(actualData)
+    }
+
+    fun get(url: String) {
+        output.write("import requests; r = requests.get('https://api.github.com/user');; r.status_code\n")
+        output.flush()
+        println("Wrote request")
+        printResponse()
+    }
 }
--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,35 +1,50 @@
 import os
 import logging
 
+import requests
+import sys
+
+class RequestsReceiver():
+    def __init__(self):
+        if not os.path.exists("/tmp/wrapperfifo_input"):
+            os.mkfifo("/tmp/wrapperfifo_input")
+        if not os.path.exists("/tmp/wrapperfifo_output"):
+            os.mkfifo("/tmp/wrapperfifo_output")
+
+        sys.stdout.write("Done\n")
+        sys.stdout.flush()
+
+        # self.input = open("/tmp/wrapperfifo_input", "r")
+        # self.output = open("/tmp/wrapperfifo_output", "w")
+
+        logging.info("Opened!")
+
+    def receive(self):
+        self.input = open("/tmp/wrapperfifo_input", "r")
+        self.output = open("/tmp/wrapperfifo_output", "w")
+
+        while True:
+            message = self.input.read(1024)
+            logging.info("Received: {}".format(message))
+            if len(message) == 0:
+                logging.info("Empty")
+                break
+            if message == "exit":
+                logging.info("Exit")
+                break
+            exec_part, eval_part = message.split(";;")
+            exec(exec_part)
+            res = eval(eval_part, globals(), locals())
+            logging.info("Result is {}".format(res))
+            self.output.write(str(res))
+            self.output.flush()
+            logging.info("Wrote")
+
+        logging.info("Exit")
+        self.input.close()
+        self.output.close()
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-
-    if not os.path.exists("/tmp/wrapperfifo_input"):
-        os.mkfifo("/tmp/wrapperfifo_input")
-    if not os.path.exists("/tmp/wrapperfifo_output"):
-        os.mkfifo("/tmp/wrapperfifo_output")
-
-    input = open("/tmp/wrapperfifo_input", "r")
-    output = open("/tmp/wrapperfifo_output", "w")
-
-    logging.info("Opened!")
-
-    while True:
-        message = input.read(1024)
-        logging.info("Received")
-        if len(message) == 0:
-            logging.info("Empty")
-            break
-        if message == "exit":
-            logging.info("Exit")
-            break
-        print(message)
-        res = eval(message)
-        logging.info("Result is {}".format(res))
-        output.write(res)
-        output.flush()
-        logging.info("Wrote")
-
-    logging.info("Exit")
-    input.close()
-    output.close()
+    receiver = RequestsReceiver()
+    receiver.receive()
