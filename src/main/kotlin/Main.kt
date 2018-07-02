394b3e5f7acff4b644e8a33a8ab327a4933d1ff0
Artyom Aleksyuk <artyom.h31@gmail.com>
2018-07-02 19:40:10 +0300

    Import clean up

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,3 @@
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.KotlinModule
-import java.io.File
-import java.io.Reader
-import java.io.Writer
-import java.util.*
-import org.slf4j.LoggerFactory
-
-
 fun main(args: Array<String>) {
     val requests = Requests()
     val resp = requests.get("https://api.github.com/user")
