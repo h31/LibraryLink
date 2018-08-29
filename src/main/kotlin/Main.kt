873a64aa8a8162685e812e26dd6d2119ffc462a0
Artyom Aleksyuk <artyom.h31@gmail.com>
2018-08-29 18:45:12 +0300

    Test GC interaction

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,16 @@
 fun main(args: Array<String>) {
+    doRequest()
+    while (true) {
+        System.gc()
+        Thread.sleep(1000)
+    }
+}
+
+fun doRequest() {
     val requests = Requests()
     val resp = requests.get("https://api.github.com/user")
     println(resp.statusCode())
     println(String(resp.content()))
     println(resp.headers())
-    requests.stopPython()
+//    requests.stopPython()
 }
