64ac540933ecfe757abad2632cbbf69ab9b05d7a
Artyom Aleksyuk <artyom.h31@gmail.com>
2019-03-06 19:56:42 +0300

    Z3 model is finally able to execute a demorgan example

--- generator/src/test/kotlin/Z3Example.kt
+++ generator/src/test/kotlin/Z3Example.kt
@@ -1,7 +1,5 @@
 import org.junit.Test
-import ru.spbstu.kspt.librarylink.DummyRunner
-import ru.spbstu.kspt.librarylink.LibraryLink
-import ru.spbstu.kspt.librarylink.ProtoBufDataExchange
+import ru.spbstu.kspt.librarylink.*
 
 class Z3Example {
     init {
@@ -9,31 +7,87 @@ class Z3Example {
         LibraryLink.exchange = ProtoBufDataExchange()
     }
 
+    fun mk_solver(ctx: Z3Kotlin.Z3_context): Z3Kotlin.Z3_solver {
+        val s: Z3Kotlin.Z3_solver = ctx.Z3_mk_solver();
+        ctx.Z3_solver_inc_ref(s);
+        return s;
+    }
+
+    fun del_solver(ctx: Z3Kotlin.Z3_context, s: Z3Kotlin.Z3_solver) {
+        ctx.Z3_solver_dec_ref(s);
+    }
+
+    enum class Z3_lbool(val i: Int) {
+        Z3_L_FALSE(-1),
+        Z3_L_UNDEF(0),
+        Z3_L_TRUE(1);
+
+        companion object {
+            fun valueOf(value: Int): Z3_lbool? = Z3_lbool.values().find { it.i == value }
+        }
+    };
+
+
     @Test
     fun demorgan() {
+        val cfg: Z3Kotlin.Z3_config;
+        val ctx: Z3Kotlin.Z3_context;
+        val s: Z3Kotlin.Z3_solver;
+        val bool_sort: Z3Kotlin.Z3_sort;
+        val symbol_x: Z3Kotlin.Z3_symbol;
+        val symbol_y: Z3Kotlin.Z3_symbol;
+        val x: Z3Kotlin.Z3_ast;
+        val y: Z3Kotlin.Z3_ast
+        val not_x: Z3Kotlin.Z3_ast
+        val not_y: Z3Kotlin.Z3_ast
+        val x_and_y: Z3Kotlin.Z3_ast
+        val ls: Z3Kotlin.Z3_ast
+        val rs: Z3Kotlin.Z3_ast
+        val conjecture: Z3Kotlin.Z3_ast
+        val negated_conjecture: Z3Kotlin.Z3_ast;
+        val args = makeArray<Z3Kotlin.Z3_ast>(2);
+
         println("DeMorgan");
 
-        val cfg            = Z3Kotlin.Z3_config();
-        val ctx            = cfg.Z3_mk_context();
+        cfg                = Z3Kotlin.Z3_config();
+        ctx                = cfg.Z3_mk_context();
         cfg.Z3_del_config();
-        val bool_sort      = ctx.Z3_mk_bool_sort();
-        val symbol_x       = ctx.Z3_mk_int_symbol(0);
-        val symbol_y       = ctx.Z3_mk_int_symbol(1);
-        val x              = ctx.Z3_mk_const(symbol_x, bool_sort);
-        val y              = ctx.Z3_mk_const(symbol_y, bool_sort);
+        bool_sort          = ctx.Z3_mk_bool_sort();
+        symbol_x           = ctx.Z3_mk_int_symbol(0);
+        symbol_y           = ctx.Z3_mk_int_symbol(1);
+        x                  = ctx.Z3_mk_const(symbol_x, bool_sort);
+        y                  = ctx.Z3_mk_const(symbol_y, bool_sort);
 
         /* De Morgan - with a negation around */
         /* !(!(x && y) <-> (!x || !y)) */
-        val not_x          = ctx.Z3_mk_not(x);
-        val not_y          = ctx.Z3_mk_not(y);
+        not_x              = ctx.Z3_mk_not(x);
+        not_y              = ctx.Z3_mk_not(y);
         args[0]            = x;
         args[1]            = y;
-        val x_and_y        = ctx.Z3_mk_and(2, args);
-        ls                 = Z3_mk_not(ctx, x_and_y);
+        x_and_y            = ctx.Z3_mk_and(2, args);
+        ls                 = ctx.Z3_mk_not(x_and_y);
         args[0]            = not_x;
         args[1]            = not_y;
-        rs                 = Z3_mk_or(ctx, 2, args);
-        conjecture         = Z3_mk_iff(ctx, ls, rs);
-        negated_conjecture = Z3_mk_not(ctx, conjecture);
+        rs                 = ctx.Z3_mk_or(2, args);
+        conjecture         = ctx.Z3_mk_iff(ls, rs);
+        negated_conjecture = ctx.Z3_mk_not(conjecture);
+
+        s = mk_solver(ctx);
+        ctx.Z3_solver_assert(s, negated_conjecture);
+        val result = Z3_lbool.valueOf(ctx.Z3_solver_check(s))
+        println("Result is $result")
+        when (result) {
+            Z3_lbool.Z3_L_FALSE ->
+            /* The negated conjecture was unsatisfiable, hence the conjecture is valid */
+            print("DeMorgan is valid\n");
+            Z3_lbool.Z3_L_UNDEF ->
+            /* Check returned undef */
+            print("Undef\n");
+            Z3_lbool.Z3_L_TRUE ->
+            /* The negated conjecture was satisfiable, hence the conjecture is not valid */
+            print("DeMorgan is not valid\n");
+        }
+        del_solver(ctx, s);
+        ctx.Z3_del_context();
     }
 }
--- models/library/Z3.lsl
+++ models/library/Z3.lsl
@@ -10,12 +10,12 @@ library Z3 {
    Z3_sort (Z3_sort);
    Z3_symbol (Z3_symbol);
    Z3_ast (Z3_ast);
-   Z3_ast_pair (Z3_ast[]);
+   Z3_solver (Z3_solver);
+   Z3_lbool (Z3_lbool);
  }
 
  converters {
-   Content <- <Response>.content();
-   Headers <- <Response>.headers();
+   Z3_ast_size <- sizeof(Z3_ast);
  }
 
  automaton Z3_config {
@@ -25,38 +25,51 @@ library Z3 {
    shift Constructed -> Closed (Z3_del_config);
  }
 
-   fun Z3_config.Z3_mk_config(): Z3_config;
-   fun Z3_config.Z3_mk_context(cfg: self): Z3_context;
-   fun Z3_config.Z3_del_config(cfg: self);
+ fun Z3_config.Z3_mk_config(): Z3_config;
+ fun Z3_config.Z3_mk_context(cfg: self): Z3_context;
+ fun Z3_config.Z3_del_config(cfg: self);
 
-  automaton Z3_context {
-    state Constructed;
-    shift Constructed -> self (Z3_mk_bool_sort);
-    shift Constructed -> self (Z3_mk_int_symbol, Z3_mk_const, Z3_mk_and, Z3_mk_or, Z3_mk_not, Z3_mk_iff);
-  }
+ automaton Z3_context {
+   state Constructed;
+   shift Constructed -> self (Z3_mk_bool_sort);
+   shift Constructed -> self (Z3_mk_int_symbol, Z3_mk_const, Z3_mk_and, Z3_mk_or, Z3_mk_not, Z3_mk_iff);
+   shift Constructed -> self (Z3_mk_solver, Z3_solver_inc_ref, Z3_solver_dec_ref);
+   shift Constructed -> self (Z3_solver_assert, Z3_solver_check);
+   shift Constructed -> Closed (Z3_del_context);
+ }
 
-  fun Z3_context.Z3_mk_bool_sort(cfg: self): Z3_sort;
-  fun Z3_context.Z3_mk_int_symbol(cfg: self, i: Int): Z3_symbol;
+ fun Z3_context.Z3_mk_bool_sort(cfg: self): Z3_sort;
+ fun Z3_context.Z3_mk_int_symbol(cfg: self, i: Int): Z3_symbol;
 
-    automaton Z3_sort {
-      state Constructed;
-    }
+ automaton Z3_sort {
+   state Constructed;
+ }
 
-  automaton Z3_symbol {
-    state Created, Constructed;
-  }
+ automaton Z3_symbol {
+   state Created, Constructed;
+ }
 
-  automaton Z3_ast {
-    state Created, Constructed;
-  }
-    
-  automaton Z3_ast_pair {
-    state Constructed;
-  }
+ automaton Z3_ast {
+   state Created, Constructed;
+ }
+ 
+ automaton Z3_solver {
+   state Created, Constructed;
+ }
 
-  fun Z3_context.Z3_mk_const(cfg: self, s: Z3_symbol, ty: Z3_sort): Z3_ast;
-  fun Z3_context.Z3_mk_and(cfg: self, num_args: Int, args: Z3_ast_pair): Z3_ast;
-  fun Z3_context.Z3_mk_or(cfg: self, num_args: Int, args: Z3_ast_pair): Z3_ast;
-  fun Z3_context.Z3_mk_not(cfg: self, a: Z3_ast): Z3_ast;
-  fun Z3_context.Z3_mk_iff(cfg: self, t1: Z3_ast, t2: Z3_ast): Z3_ast;
+ fun Z3_context.Z3_mk_const(cfg: self, s: Z3_symbol, ty: Z3_sort): Z3_ast;
+ fun Z3_context.Z3_mk_and(cfg: self, num_args: Int, args: Z3_ast[]): Z3_ast;
+ fun Z3_context.Z3_mk_or(cfg: self, num_args: Int, args: Z3_ast[]): Z3_ast;
+ fun Z3_context.Z3_mk_not(cfg: self, a: Z3_ast): Z3_ast;
+ fun Z3_context.Z3_mk_iff(cfg: self, t1: Z3_ast, t2: Z3_ast): Z3_ast;
+ fun Z3_context.Z3_mk_iff(cfg: self, t1: Z3_ast, t2: Z3_ast): Z3_ast;
+  
+ fun Z3_context.Z3_mk_solver(cfg: self): Z3_solver;
+ fun Z3_context.Z3_solver_inc_ref(cfg: self, s: Z3_solver);
+ fun Z3_context.Z3_solver_dec_ref(cfg: self, s: Z3_solver);
+ 
+ fun Z3_context.Z3_solver_assert(cfg: self, s: Z3_solver, a: Z3_ast);
+ fun Z3_context.Z3_solver_check(cfg: self, s: Z3_solver): Int;
+ 
+ fun Z3_context.Z3_del_context(cfg: self);
 }
