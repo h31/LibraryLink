f4dda7e2d8d7d34a11c68c18117a3d05d7f37d5d
Artyom Aleksyuk <artyom.h31@gmail.com>
2019-03-20 22:36:37 +0300

    Much better type handling in the generator

--- generator/src/main/kotlin/ru/spbstu/kspt/librarylink/generator/GeneratorST.kt
+++ generator/src/main/kotlin/ru/spbstu/kspt/librarylink/generator/GeneratorST.kt
@@ -12,96 +12,59 @@ import com.github.ajalt.clikt.parameters.types.file
 import org.stringtemplate.v4.ST
 import org.stringtemplate.v4.STGroupFile
 import ru.spbstu.kspt.librarylink.MethodCallRequest
-import ru.spbstu.kspt.librarylink.Request
 import ru.spbstu.kspt.librarymigration.parser.*
 import java.io.File
 import java.io.InputStream
 
-val primitiveTypes = listOf("String", "int", "Int")
-var supportsOOP = false
+var explicitSelf = false
 
-private fun generateArgs(args: List<FunctionArgument>, types: Map<String, String>): List<Arg> {
-    val validArgs = if (supportsOOP) args.filterNot { it.type == "self" } else args
+private fun generateArgs(args: List<FunctionArgument>, types: List<TypeDecl>): List<Arg> {
+    val validArgs = if (explicitSelf) args else args.filterNot { it.type.isSelf() }
     val generated = validArgs.map {
-        Arg(type = checkNotNull(types[it.type]),
-                name = it.name, reference = calcIsReference(types[it.type]!!), self = it.type == "self")
+        Arg(type = types[it.type].codeType.typeName,
+                name = it.name, reference = types[it.type].semanticType.isReference(), self = it.type.isSelf())
     } // TODO
     generated.filterNot { it.self }.withIndex().forEach { it.value.index = it.index }
     return generated
 }
 
-const val cStyleArrayType = "%s*"
-const val kotlinStyleArrayType = "ArrayHandle<%s>"
+val cStyleArrayType = { type: CodeType -> CodeType("${type.typeName}*") }
+val kotlinStyleArrayType = { type: CodeType -> CodeType("ArrayHandle<${type.typeName.capitalize()}>") }
 
-fun replaceArrayType(types: Map<String, String>): Map<String, String> {
-    return types.mapValues { if ("[]" in it.value) {
-        "ArrayHandle<${it.value.replace("[]", "")}>"
-    } else {
-        it.value
-    } }
-}
+val arrayTypeConverters = mapOf("kotlin" to kotlinStyleArrayType, "cpp" to cStyleArrayType)
 
-fun patchLibraryDeclForArrays(library: LibraryDecl, types: Map<String, String>): LibraryDecl {
-    val arrayTypes = getArrayTypes(library)
-    val typeDecls = arrayTypes.map { type ->
-        val itemType = checkNotNull(types[type.removeSuffix("[]")])
-        val formatString = if (supportsOOP) kotlinStyleArrayType else cStyleArrayType
-        Type(semanticType = type, codeType = formatString.format(itemType))
-    }
-    return library.copy(types = library.types + typeDecls)
-}
+fun List<TypeDecl>.withSelfTypeDecl(entity: SemanticType) = this + TypeDecl(semanticType = SemanticType("self"), codeType = this[entity].codeType)
 
-fun detectArraysInTypes(library: LibraryDecl, types: Map<String, String>): List<WrappedClass> {
-    val arrayTypes = getArrayTypes(library)
-    val arrayClasses = mutableListOf<WrappedClass>()
-    for (type in arrayTypes) {
-        val itemType = checkNotNull(types[type.removeSuffix("[]")])
-        val arrayType = "$itemType[]"
-        val set = Method("set<$itemType>", args = listOf())
-        set.request = set.request.copy(type = arrayType)
-        val get = Method("get<$itemType>", args = listOf())
-        get.request = get.request.copy(type = arrayType)
-        val memAlloc = Method("mem_alloc<$itemType>", args = listOf())
-        memAlloc.request = memAlloc.request.copy(type = arrayType)
-        arrayClasses += WrappedClass(name = type, methods = listOf(),
-                builtinMethods = listOf(set, get, memAlloc), constructor = null)
-//        val arrayAutomaton = Automaton(name = type,
-//                states = listOf(StateDecl("Created"), StateDecl("Constructed")),
-//                shifts = listOf(), extendable = false)
-    }
-    return arrayClasses
-}
+fun SemanticType.isSelf() = typeName == "self"
 
-private fun getArrayTypes(library: LibraryDecl): Collection<String> =
-        library.functions.flatMap { it.args }.map { it.type }.filter { it.contains("[]") }.toSet()
-
-fun generateST(srcLibrary: LibraryDecl, st: ST) {
-    var types = srcLibrary.types.map { it.semanticType to it.codeType }.toMap()
-
-    val library = patchLibraryDeclForArrays(srcLibrary, types)
-
-    types = library.types.map { it.semanticType to it.codeType }.toMap()
+fun generateST(srcLibrary: LibraryDecl, st: ST, destinationLanguage: String) {
+    val library = srcLibrary
+            .addMissingAutomata()
+            .addDefaultStates()
+            .addArrayTypeDecls(checkNotNull(arrayTypeConverters[destinationLanguage]))
+            .generateHandlersForArrayTypes()
 
     val methods = mutableMapOf<String, Method>()
 
     for (function in library.functions) {
-        val hasSelf = function.args.any { it.type == "self" }
+        val typesWithSelf = library.types.withSelfTypeDecl(function.entity)
+        val hasSelf = function.args.any { it.type.isSelf() }
+        val hasReturnValue = function.returnValue != null
         val static = function.staticName != null || !hasSelf
         val objectId = if (static) function.staticName?.staticName ?: "" else "assignedID"
         val property = function.properties.any { it.key == "type" && it.value == "get" }
-        val args = generateArgs(function.args, types + ("self" to function.entity))
+        val args = generateArgs(function.args, typesWithSelf)
         val request = MethodCallRequest(
                 methodName = function.name,
-                type = function.entity,
+                type = function.entity.typeName,
                 objectID = objectId,
                 args = listOf(),
-                doGetReturnValue = function.returnValue != null,
+                doGetReturnValue = hasReturnValue,
                 isProperty = property,
                 isStatic = static)
-        val method = Method(function.name, args)
-        method.request = request
-        method.returnValue = types[function.returnValue] // TODO: Check
-        method.referenceReturn = method.returnValue !in primitiveTypes
+        val returnType = if (hasReturnValue) typesWithSelf[function.returnValue!!].codeType.typeName else null // TODO: Check
+        val method = Method(name = function.name, args = args, request = request,
+                returnValue = returnType, referenceReturn = function.returnValue?.isReference() ?: false)
         methods += method.name to method
     }
 
@@ -109,8 +72,8 @@ fun generateST(srcLibrary: LibraryDecl, st: ST) {
         if (automaton.extendable) {
             val classMethods = automaton.shifts.flatMap { it.functions }.map { checkNotNull(methods[it]) }
             if (classMethods.size == 1) { // Callback
-                val method = classMethods.single()
-                method.clazz = WrappedClass(automaton.name, listOf(), listOf(), null)
+                val srcMethod = classMethods.single()
+                val method = srcMethod.copy(clazz = WrappedClass(automaton.name.typeName, listOf(), listOf(), null))
                 st.add("callbacks", method)
             } else {
                 TODO()
@@ -119,7 +82,7 @@ fun generateST(srcLibrary: LibraryDecl, st: ST) {
             val classMethods = automaton.shifts.filterNot { it.from == "Created" }.flatMap { it.functions }.map { checkNotNull(methods[it]) }
             val constructor = automaton.shifts.filter { it.from == "Created" }.flatMap { it.functions }.map { checkNotNull(methods[it]) }.singleOrNull()
             val clazz = WrappedClass(
-                    automaton.name,
+                    automaton.name.typeName,
                     classMethods,
                     listOf(),
                     constructor
@@ -128,7 +91,6 @@ fun generateST(srcLibrary: LibraryDecl, st: ST) {
             st.add("wrappedClasses", clazz)
         }
     }
-    detectArraysInTypes(library, types).forEach { st.add("builtinClasses", it) }
     st.add("libraryName", library.name)
 //    val wrappedClass = WrappedClass(
 //            "Requests",
@@ -177,8 +139,6 @@ fun generateST(srcLibrary: LibraryDecl, st: ST) {
 //    println(code)
 }
 
-fun calcIsReference(type: String) = type !in primitiveTypes
-
 data class WrappedClass(val name: String,
                         val methods: List<Method>,
                         val builtinMethods: List<Method>,
@@ -186,12 +146,9 @@ data class WrappedClass(val name: String,
     var noExplicitConstructors: Boolean = true
 }
 
-data class Method(val name: String, val args: List<Arg>) {
-    var request: MethodCallRequest = MethodCallRequest("methodName", "ObjectID", "", listOf(),  true, false, true)
-    var returnValue: String? = null
-    var referenceReturn: Boolean = true
-    var clazz: WrappedClass? = null
-}
+data class Method(val name: String, val args: List<Arg>,
+                  val request: MethodCallRequest, var returnValue: String? = null,
+                  var referenceReturn: Boolean = true, val clazz: WrappedClass? = null) // , val returnValueConstructor: String?
 
 data class Arg(val type: String, val name: String, val reference: Boolean, val self: Boolean) {
     var index = 0
@@ -200,10 +157,10 @@ data class Arg(val type: String, val name: String, val reference: Boolean, val s
 fun InputStream.parseModel() = ModelParser().parse(this)
 
 class GeneratorCommand : CliktCommand() {
-    val language: String by option("-l", "--language", help="Receiver language").choice(*supportedLanguages()).required()
+    val language: String by option("-l", "--language", help = "Receiver language").choice(*supportedLanguages()).required()
     val explicitSelf: Boolean by option("-s").flag()
     val output: File? by option("-o", "--output", help = "Output file").file()
-    val models: List<File> by argument(help="Models").file(exists = true).multiple(required = true).validate {
+    val models: List<File> by argument(help = "Models").file(exists = true).multiple(required = true).validate {
         require(it.isNotEmpty()) { "At least one model should be specified" }
     }
 
@@ -211,7 +168,8 @@ class GeneratorCommand : CliktCommand() {
             template = "generator/$language.stg",
             modelFiles = models,
             outputFile = output,
-            explicitSelf = explicitSelf
+            explicitSelfArg = explicitSelf,
+            destinationLanguage = language
     )
 
     private fun resourceReader(name: String) = this.javaClass.classLoader.getResourceAsStream(name).bufferedReader()
@@ -222,8 +180,8 @@ class GeneratorCommand : CliktCommand() {
 
 fun main(args: Array<String>) = GeneratorCommand().main(args)
 
-private fun generateWrapper(template: String, modelFiles: List<File>, outputFile: File?, explicitSelf: Boolean) {
-    supportsOOP = !explicitSelf
+private fun generateWrapper(template: String, modelFiles: List<File>, outputFile: File?, explicitSelfArg: Boolean, destinationLanguage: String) {
+    explicitSelf = explicitSelfArg
     val group = STGroupFile(template)
     val st = group.getInstanceOf("wrapperClass")
 //    st.add("type", "int")
@@ -243,7 +201,7 @@ private fun generateWrapper(template: String, modelFiles: List<File>, outputFile
 //    st.add("value", 0)
     val models = modelFiles.map { it.inputStream().parseModel() }
     val mergedAST = mergeASTs(models.first().name, models)
-    generateST(mergedAST, st)
+    generateST(mergedAST, st, destinationLanguage)
     val result = st.render()
     if (outputFile != null) {
         outputFile.writeText(result)
